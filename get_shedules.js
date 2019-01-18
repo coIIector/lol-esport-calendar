d0bfadf82e339891ed3a02d1b949b17aafc60dbf
coIIector <coiiector@users.noreply.github.com>
2019-01-19 00:21:18 +0100

    iCal generating

--- get_shedules.js
+++ get_shedules.js
@@ -10,12 +10,12 @@ const URL_PARAMS = {
 };
 
 const LEAGUES = [
-    // '98767991299243165', // LCS
-    // '98767991302996019', // LEC
+    '98767991299243165', // LCS
+    '98767991302996019', // LEC
     '98767991310872058', // LCK
     // '98767991314006698', // LPL
     // '99332500638116286', // NA ACADEMY
-    // '98767975604431411', // WORLDS
+    '98767975604431411', // WORLDS
     // '98767991295297326', // ALL STARS
 ];
 URL_PARAMS.leagueId = LEAGUES.join(',');
@@ -38,6 +38,11 @@ const httpClient = request.defaults({
 });
 
 const allEvents = [];
+let calendarString = `BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:github.com/coIIector/lol-esport-calendar
+NAME:LoL esport
+X-WR-CALNAME:LoL esport`;
 
 function getNextPage(pageToken) {
     if (pageToken)
@@ -60,7 +65,38 @@ function getNextPage(pageToken) {
 }
 
 function generateCalendar() {
-    console.log(allEvents);
+    allEvents.forEach(function (event) {
+        const ts = new Date(Date.parse(event.startTime));
+        const te = new Date(Date.parse(event.startTime) + 1000 * 60 * 60);
+
+        calendarString += "\nBEGIN:VEVENT";
+        calendarString += "\nDTSTART:" + formatDate(ts);
+        // calendarString += "\nDTEND:" + formatDate(te);
+        calendarString += "\nSUMMARY:" + event.match.teams[0].code + " vs. " + event.match.teams[1].code;
+        calendarString += "\nDESCRIPTION:" + event.league.name + " " + event.blockName + " | VOD: " + "https://watch.na.lolesports.com/vod/" + event.match.id;
+        calendarString += "\nURL:" + "https://watch.na.lolesports.com/vod/" + event.match.id;
+        calendarString += "\nEND:VEVENT";
+    });
+
+    calendarString += "\nEND:VCALENDAR";
+
+    console.log(calendarString);
+}
+
+function formatDate(date) {
+    function prependZero(number) {
+        if (number < 10) {
+            return "0" + number;
+        } else {
+            return number;
+        }
+    }
+
+    return "" + date.getUTCFullYear()
+        + prependZero(date.getUTCMonth() + 1)
+        + prependZero(date.getUTCDate()) + "T"
+        + prependZero(date.getUTCHours())
+        + prependZero(date.getUTCMinutes()) + "00Z";
 }
 
 getNextPage();
